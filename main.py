32a2221aba9466b860f7321e4143cf4eaf18c684
diligejy <sjy049@gmail.com>
2022-07-24 22:08:14 +0900

    codingapple fastapi

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return FileResponse('index.html')
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+class Model(BaseModel):
+    name : str 
+    phone : int
+
+@app.post("/send")
+def send_info(data: Model):
+    print(data)
+    return '전송완료'
